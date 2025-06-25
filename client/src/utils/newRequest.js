64226a51fd9e22983d97aca3371b2f9384c70a3b
Veeras-code <nagasai692004@gmail.com>
2025-06-25 14:29:18 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const newRequest = axios.create({
   baseURL: "https://kisanmart-backend.onrender.com/api/",
   //baseURL: "https://api-h2x3.onrender.com/api/",
-//  baseURL: "https://agrosync-prototype.vercel.app/api/",
   withCredentials: true,
 });
 
